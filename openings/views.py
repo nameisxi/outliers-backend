1a100f8cf7d0ac759a5dea5f58a16726489ea5c9
Tuomas Peltonen <tuomas.j.j.peltonen@gmail.com>
2022-05-14 20:38:42 +0900

    Added new fields and error handling to create opening view

--- openings/views.py
+++ openings/views.py
@@ -14,30 +14,46 @@ class CreateOpeningView(APIView):
         employee = Employee.objects.get(user=request.user)
         company = Company.objects.get(employees=employee)
 
+        print()
+        print("DATA:", request.data)
+        print()
+
         opening = Opening(
             company=company,
             created_by=employee,
             status='Sourcing',
-            title=request.data['title'],
-            team=request.data['team'],
-            description=request.data['description'],
-            years_of_experience=request.data['years_of_experience'],
-            base_compensation=request.data['base_compensation'],
-            equity_compensation=request.data['equity_compensation'],
-            other_compensation=request.data['other_compensation'],
+            title=request.data.get('title'),
+            team=request.data.get('team', None),
+            description=request.data.get('description', None),
+            years_of_experience=request.data.get('years_of_experience', None),
+            base_compensation_min=request.data.get('base_compensation_min', None),
+            base_compensation_max=request.data.get('base_compensation_max', None),
+            base_compensation_currency=request.data.get('base_compensation_curency', None),
+            equity_compensation_min=request.data.get('equity_compensation_min', None),
+            equity_compensation_max=request.data.get('equity_compensation_max', None),
+            equity_compensation_currency=request.data.get('equity_compensation_curency', None),
+            other_compensation_min=request.data.get('other_compensation_min', None),
+            other_compensation_max=request.data.get('other_compensation_max', None),
+            other_compensation_currency=request.data.get('other_compensation_curency', None),
         )
 
-        for programming_language in request.data['programming_languages']:
-            language = ProgrammingLanguage.get(name=programming_language)
-            opening.programming_languages.add(language)
+        programming_languages = request.data.get('programming_languages', None)
+        if programming_languages:
+            for programming_language in programming_langugages:
+                language = ProgrammingLanguage.get(name=programming_language)
+                opening.programming_languages.add(language)
 
-        for technology in request.data['technologies']:
-            technology = Technology.get(name=technology)
-            opening.technologies.add(technology)
+        technologies = request.data.get('technologies', None)
+        if technologies:
+            for technology in technologies:
+                technology = Technology.get(name=technology)
+                opening.technologies.add(technology)
 
-        for topic in request.data['topics']:
-            topic = Topic.get(name=topic)
-            opening.topics.add(topic)
+        topics = request.data.get('topics', None)
+        if topics:
+            for topic in topics:
+                topic = Topic.get(name=topic)
+                opening.topics.add(topic)
 
         opening.save()
 
