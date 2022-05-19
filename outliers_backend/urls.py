3625086e6bceb27322c2115dfbf65ca2f1fb8cda
Tuomas Peltonen <tuomas.j.j.peltonen@gmail.com>
2022-05-19 20:02:54 +0900

    Added correct static folder settings for admin page styling to work

--- outliers_backend/urls.py
+++ outliers_backend/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('users/', include('users.urls')),
@@ -23,4 +25,4 @@ urlpatterns = [
     path('technologies/', include('technologies.urls')),
     path('openings/', include('openings.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
