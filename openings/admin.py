30790c0aa8b8d7b207d222f402dd1e3c1aa4571d
Tuomas Peltonen <tuomas.j.j.peltonen@gmail.com>
2022-06-01 18:21:09 +0900

    Added django admin customizations

--- openings/admin.py
+++ openings/admin.py
@@ -3,4 +3,12 @@ from django.contrib import admin
 from .models import *
 
 
-admin.site.register(Opening)
+class OpeningAdmin(admin.ModelAdmin):
+    list_display = ('get_company', 'title', 'status', 'years_of_experience_min', 'years_of_experience_max', 'created_at', 'updated_at', )
+
+    @admin.display()
+    def get_company(self, obj):
+        return obj.company.name
+
+
+admin.site.register(Opening, OpeningAdmin)
--- users/admin.py
+++ users/admin.py
@@ -8,11 +8,32 @@ from .models import *
 class CustomUserAdmin(UserAdmin):
     list_display = ('username', 'email', 'is_staff', 'last_login')
 
+
+class CompanyDomainNameAdmin(admin.ModelAdmin):
+    list_display = ('name', 'get_company', 'created_at', 'updated_at')
+
+    @admin.display()
+    def get_company(self, obj):
+        return obj.company.name
+
+class CompanyDomainNameInline(admin.TabularInline):
+    model = CompanyDomainName
+
+
 class CompanyAdmin(admin.ModelAdmin):
     list_display = ('name', 'created_at', 'updated_at')
+    fields = ('name', )
+    inlines = [
+        CompanyDomainNameInline,
+    ]
+
 
 class EmployeeAdmin(admin.ModelAdmin):
     list_display = ('user', 'company', 'created_at', 'updated_at', 'last_visit')
+
+    @admin.display()
+    def get_company(self, obj):
+        return obj.company.name
 
 
 admin.site.unregister(User)
@@ -23,6 +44,6 @@ admin.site.register(CandidateProfile)
 admin.site.register(CandidatePreProfile)
 
 admin.site.register(Company, CompanyAdmin)
-admin.site.register(CompanyDomainName)
+admin.site.register(CompanyDomainName, CompanyDomainNameAdmin)
 
 admin.site.register(Employee, EmployeeAdmin)
